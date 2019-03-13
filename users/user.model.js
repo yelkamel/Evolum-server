0749bc4996f186b41b16fb5a820da85177c7ec06
EL KAMEL <yelkamel@gmail.com>
2019-03-13 19:09:31 +0100

    change user model

--- users/user.model.js
+++ users/user.model.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    username: { type: String, unique: true, required: true },
-    hash: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    createdDate: { type: Date, default: Date.now }
+  mail: { type: String, unique: true, required: true },
+  firstName: { type: String, required: true },
+  password: { type: String, required: true },
+  hash: { type: String, required: true },
+  createdDate: { type: Date, default: Date.now }
 });
 
 schema.set('toJSON', { virtuals: true });
